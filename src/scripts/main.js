3a07bd410094617a6e1d1c0fcb2b89a6b93993a7
Markus Wanjura <wanjura@develop4edu.de>
2015-06-08 09:53:54 +0200

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,32 @@
+// Require Node modules in the browser thanks to Browserify: http://browserify.org
+var bespoke = require('bespoke'),
+    cube = require('bespoke-theme-cube'),
+    keys = require('bespoke-keys'),
+    touch = require('bespoke-touch'),
+    scale = require('bespoke-scale'),
+    hash = require('bespoke-hash'),
+    progress = require('bespoke-progress'),
+    bullets = require('bespoke-bullets'),
+    classes = require('bespoke-classes'),
+    backdrop = require('bespoke-backdrop'),
+    state = require('bespoke-state');
+
+// Bespoke.js
+bespoke.from('article', [
+    cube(),
+    keys(),
+    touch(),
+    scale(),
+    hash(),
+    progress(),
+    backdrop(),
+    classes(),
+    bullets(),
+    state()
+]);
+
+// Prism syntax highlighting
+// This is actually loaded from "bower_components" thanks to
+// debowerify: https://github.com/eugeneware/debowerify
+require('prism');
+
